ebf8a30d5184f5058af931ed23d3622855c48f8c
Voxic <m.hille@voxicgames.com>
2025-10-02 11:56:19 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿abstract class Animal
+{
+    public string Category { get; init; }
+    public void Breath()
+    {
+        Console.WriteLine("Is breathing....");
+    }
+}
+
+class Cow : Animal
+{
+    private readonly BreastFeedingComponent breastFeedingComponent = new BreastFeedingComponent();
+    public void EatGrass()
+    {
+        Console.WriteLine("Is eating grass...");
+        this.breastFeedingComponent.Feed();
+    }
+}
+
+class Platypus : Animal
+{
+    private readonly BreastFeedingComponent _breastFeedingComponent = new BreastFeedingComponent();
+    private readonly EggLayingComponent _eggLayingComponent = new EggLayingComponent();
+    public void EatInsects()
+    {
+        Console.WriteLine("Is eating insects...");
+    }
+}
+
+class EggLayingComponent
+{
+    public void Lay()
+    {
+        Console.WriteLine("Laying egg...");
+    }
+}
+
+class BreastFeedingComponent
+{
+    public void Feed()
+    {
+        Console.WriteLine("Schlüüürff...");
+    }
+}
