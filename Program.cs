3e948ab1413bc741318bc598edcf1fbb178e0edd
Voxic <m.hille@voxicgames.com>
2025-10-22 11:18:24 +0200

    Introduced ability runner with support for delayed and sequential combat actions. Updated main loop to use new abilities and added folder structure for better organization. Combining composite and decorator pattern at hand.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,72 @@
-﻿using PV.ObjectPooling;
+﻿var firstAttackSequence = new AbilityRunner();
+firstAttackSequence.AddAbilities(new FireBall());
+firstAttackSequence.AddAbilities(new AbilityDelay(new Summoner()) { DelayInSeconds = 3 });
+firstAttackSequence.AddAbilities(new Defend());
 
-var bulletPool = new Pool<Bullet>(20);
+var combatChain = new AbilityRunner();
+combatChain.AddAbilities(new Defend());
+combatChain.AddAbilities(new Summoner());
+combatChain.AddAbilities(new AbilityDelay(firstAttackSequence) { DelayInSeconds = 5 });
 
-var counter = 0;
 while (true)
 {
-    var bullet = bulletPool.Get();
-    bullet.Penetration = 10;
-    bullet.Strength = counter++;
-    Console.WriteLine($"Shooting bullet. Strength: {bullet.Strength} Penetration: {bullet.Penetration}");
-    bulletPool.Return(bullet);
+    if (Console.ReadLine() == "f")
+        combatChain.Use();
 }
 
-
-class Bullet
+public class AbilityRunner : IAbility
 {
-    public int Penetration { get; set; }
-    public int Strength { get; set; }
+    private readonly IList<IAbility> abilities = new List<IAbility>(10);
+
+    public void AddAbilities(params IEnumerable<IAbility> abilities)
+    {
+        foreach (var ability in abilities)
+            this.abilities.Add(ability);
+    }
+
+    public void Use()
+    {
+        foreach (var ability in this.abilities)
+            ability.Use();
+    }
+}
+
+public class AbilityDelay : IAbility
+{
+    public int DelayInSeconds { get; set; }
+    private readonly IAbility abilityToExecute;
+    public AbilityDelay(IAbility ability) => this.abilityToExecute = ability;
+
+    public void Use()
+    {
+        Console.WriteLine("Delayed ability in queue....");
+        Task.Delay(this.DelayInSeconds * 1000).Wait();
+        this.abilityToExecute.Use();
+    }
+}
+
+public class Summoner : IAbility
+{
+    public void Use()
+    {
+        Console.WriteLine("Summoning");
+    }
+}
+
+public class Defend : IAbility
+{
+    public void Use()
+    {
+        Console.WriteLine("Defending");
+    }
+}
+
+public class FireBall : IAbility
+{
+    public void Use() => Console.WriteLine("Casting Fireball");
+}
+
+public interface IAbility
+{
+    void Use();
 }
--- obj/Debug/net9.0/PV.AssemblyInfo.cs
+++ obj/Debug/net9.0/PV.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b89f07965212af3ff0c92251116a432dd72636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3523201b46277d3c2e7a3f5b9590bfd12ceb94f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
